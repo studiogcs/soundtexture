fdaa1b7f9bb31d865231cd880842d46681e07401
Gabriele Sha <gabrielesha@gmail.com>
2015-10-31 15:24:31 -0700

    run_synthesis: read input wav file using scipy, numpy

--- run_synthesis.py
+++ run_synthesis.py
@@ -1,22 +1,44 @@
-import sys
-import wave
+import sys, struct, wave
+import numpy as np
+from scipy.io.wavfile import read
 
 def open_wavefile(filename):
-	print "reading " + filename 
+	print "READING: " + filename 
 	try:
-		wavefile = wave.open(filename, 'r')
+		[fs, wavefile] = read(filename, 'r')
 	except:
 		print "ERROR: could not read file"
 		sys.exit(1)
+	
+	x = np.array(wavefile,dtype=float)
+	dim = x.shape
+	num_chan = 1
+	if len(dim) > 1:
+		num_chan = dim[1]
+		for c in range(1, num_chan):
+			x[:,c] = 1. * x[:,c] / max(abs(x[:,c])) 	# normalize
+	else:
+		x = 1. * x / max(abs(x)) 	# normalize
+	num_frames = dim[0]
 
-	[num_chan, samp_width, fs, num_frames, comptype, compname] = wavefile.getparams()
-	wavefile.close()
+	print "\tsample rate: ", fs, "\n\t# samples: ", num_frames, "\n\t# channels: ", num_chan
 
-	print "\t# channels: " + num_chan + "\n\tsample width: " + samp_width + "\n\tsample rate: " + fs + "\n\t# samples: " + 
+	return x
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
 		filename = sys.argv[1]
-		open_wavefile(filename)
+		x = open_wavefile(filename)
 	else:
-		print "default"
+		print "no user input"
+		sys.exit(1)
+
+	print x
+
+
+
+
+
+
+
+
